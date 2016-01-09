76ca7ef594737db3d0c08046fbdf41f40543db94
Duke <dukekhaos@gmail.com>
2016-01-08 23:13:44 -0200

    improve test style

--- test/reducers/subscriptions.js
+++ test/reducers/subscriptions.js
@@ -4,50 +4,28 @@ import * as types from '../../src/constants/action-types'
 
 describe('subscriptions reducer', () => {
   it('should return the initial state', () => {
-    expect(
-      reducer(undefined, {})
-    ).toEqual({})
+    expect(reducer(undefined, {})).toEqual({})
   })
 
   it('should handle RECEIVE_SUBSCRIPTIONS', () => {
-    expect(
-      reducer({}, {
-        type: types.RECEIVE_SUBSCRIPTIONS,
-        channels: ['Run the tests']
-    })
-    ).toEqual(
-      {
-        channels: [ 'Run the tests' ],
-        hasError: false,
-        isFetching: false,
-        lastUpdated: undefined
-      }
-    )
+    const expected =  { channels: [ 'Run the tests' ],
+                        hasError: false,
+                        isFetching: false,
+                        lastUpdated: undefined
+                      }
+    const action = { type: types.RECEIVE_SUBSCRIPTIONS, channels: ['Run the tests'] }
+    expect(reducer({}, action)).toEqual(expected)
   })
 
-  it('should handle RECEIVE_SUBSCRIPTIONS', () => {
-    expect(
-      reducer({}, {
-        type: types.REQUEST_SUBSCRIPTIONS
-    })
-    ).toEqual(
-      {
-        isFetching: true,
-        hasError: false
-      }
-    )
+  it('should handle RECEIVE_SUBSCRIPTIONS empty', () => {
+    const expected = { isFetching: true, hasError: false }
+    const action = { type: types.REQUEST_SUBSCRIPTIONS }
+    expect(reducer({}, action)).toEqual(expected)
   })
 
   it('should handle FAILED_FETCH_SUBSCRIPTIONS', () => {
-    expect(
-      reducer({}, {
-        type: types.FAILED_FETCH_SUBSCRIPTIONS
-    })
-    ).toEqual(
-      {
-        isFetching: false,
-        hasError: true
-      }
-    )
+    const expected = { isFetching: false, hasError: true }
+    const action = { type: types.FAILED_FETCH_SUBSCRIPTIONS }
+    expect(reducer({}, action)).toEqual(expected)
   })
 })
